97fed8fd8a139b950a69303abee87d5de4f59822
mike stagg <mikecstagg@gmail.com>
2022-04-04 16:45:19 -0700

    switcheroo

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" prefix="og: http://ogp.me/ns#">
       <Head>
-        <meta name="image" property="og:image" content="spotify.png"/>
+        <meta name="image" property="og:image" content="https://www.protocol.com/media-library/spotify-app-with-headphones-on.png"/>
         <meta property="og:title" content="Spotify Lookin' Fly"/>
         <meta name="author" content="Mike Stagg" />
         <meta property="og:description" content="This is a Spotify Clone"/>

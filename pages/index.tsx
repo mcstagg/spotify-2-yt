568b39a51b9a76d08c4a9a3027c00c84fc6e1af8
mike stagg <mikecstagg@gmail.com>
2022-04-04 17:02:47 -0700

    switch

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Head>
         <title>Spotify 2.0</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="image" property="og:image" content=".//spotify.jpg"/>
+        <meta name="image" property="og:image" content="./spotify.jpg"/>
       </Head>
 
       <main className="overflow-hidden scrollbar-hide flex">
